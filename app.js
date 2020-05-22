d9d7e64a22912ba35843f96b67637ab7c9726907
Akshay Salunkhe <akshay.salunkhe705@gmail.com>
2020-05-23 01:58:53 +0530

    commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const express = require('express')
+const app = express();
+const bodyParser = require('body-parse')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1/dukan',{ useNewUrlParser:true, useUnifiedTopology:true }).then(() => {
+    console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
